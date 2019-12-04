940a19ca482e04f1008510b6824659a476021cfe
Maxim Timinskiy <timinmax@gmail.com>
2019-12-04 22:15:27 +0300

    task 5

--- hw01/src/hw01.java
+++ hw01/src/hw01.java
@@ -1,5 +1,6 @@
 public class hw01 {
     public static void main(String[] args) {
+        //task 2
         byte byteVar = 0;
         short shortVar = 0;
         int intVar = 0;
@@ -9,15 +10,22 @@ public class hw01 {
         char charVar = 'a';
         boolean boolVar = true;
 
+        //task 3
         float a,b,c,d;
         a = b = c = d = 10f;
 
         floatVar = calcFormula(a, b, c, d);
         System.out.println("" + a + " * (" + b + " + (" + c + " / " + d + ")) = " + floatVar);
 
+        //task 4
         int firstNum, secondNum;
         firstNum = secondNum = 17;
         System.out.println("Test result is " + testNumbers(firstNum, secondNum));
+
+        //task 5
+        checkSign(9);
+        checkSign(0);
+        checkSign(-8);
     }
 
     public static float calcFormula(float a, float b, float c, float d){
@@ -27,5 +35,9 @@ public class hw01 {
     public static boolean testNumbers(int firstNumber, int secondNumber){
         int sumOfNum = firstNumber + secondNumber;
         return sumOfNum>=10 && sumOfNum<=20;
+    }
+
+    public static void checkSign(int number2Check){
+        System.out.println("The number " + number2Check + " is " + ((number2Check>=0)?"positive.":"negative"));
     }
 }
