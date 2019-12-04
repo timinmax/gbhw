bf78dca4fe3444ac783eb883cde6374694def94b
Maxim Timinskiy <timinmax@gmail.com>
2019-12-04 21:56:06 +0300

    task 1-3

--- hw01/src/hw01.java
+++ hw01/src/hw01.java
@@ -1,5 +1,22 @@
 public class hw01 {
     public static void main(String[] args) {
-        System.out.println("Hello, git");
+        byte byteVar = 0;
+        short shortVar = 0;
+        int intVar = 0;
+        long longVar = 0L;
+        float floatVar = 0f;
+        double doubleVar = 0;
+        char charVar = 'a';
+        boolean boolVar = true;
+
+        float a,b,c,d;
+        a = b = c = d = 10f;
+
+        floatVar = calcFormula(a, b, c, d);
+        System.out.println("" + a + " * (" + b + " + (" + c + " / " + d + ")) = " + floatVar);
+    }
+
+    public static float calcFormula(float a, float b, float c, float d){
+        return a * (b + (c / d));
     }
 }
