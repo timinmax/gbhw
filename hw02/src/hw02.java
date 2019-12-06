96c42e49984a3d357fafcbbfe2d8c8b9f287aab0
Maxim Timinskiy <timinmax@gmail.com>
2019-12-06 22:55:18 +0300

    task 02_04

--- hw02/src/hw02.java
+++ hw02/src/hw02.java
@@ -1,20 +1,26 @@
 public class hw02 {
     public static void main(String[] args) {
-        //Домашнее задание 02 задача 01
+        System.out.println("Домашнее задание 02 задача 01");
         int[] testArr = {0,1,0,1,1,1,1,0,1,1};
         print_r(testArr);
         int[] invertedArr = inverter(testArr);
         print_r(invertedArr);
 
-        //Домашнее задание 02 задача 02
+        System.out.println("\nДомашнее задание 02 задача 02");
         int[] arrayStep3 = getArrayIncrStep(3, 8);
         print_r(arrayStep3);
 
-        //Домашнее задание 02 задача 03
+        System.out.println("\nДомашнее задание 02 задача 03");
         int[] anotherTestArr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         print_r(anotherTestArr);
         processArray(anotherTestArr);
         print_r(anotherTestArr);
+
+        System.out.println("\nДомашнее задание 02 задача 04");
+        int[][] table = getQuadTable(8);
+        for (int i = 0; i<table.length; i++){
+            print_r(table[i]);
+        }
     }
 
 
@@ -38,6 +44,14 @@ public class hw02 {
         for (int i = 0; i<array2Process.length; i++){
             array2Process[i] = array2Process[i] * ((array2Process[i]<6)?2:1);
         }
+    }
+
+    private static int[][] getQuadTable(int size){
+        int[][] returnTable = new int[size][size];
+        for (int i = 0; i<size; i++){
+            returnTable[i][i] = 1;
+        }
+        return returnTable;
     }
 
     private static void print_r(int[] arrayToPrint){
