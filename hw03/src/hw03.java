3d1184297300f6192ae231dd63a20177891fd123
Maxim Timinskiy <timinmax@gmail.com>
2019-12-11 22:44:01 +0300

    Guess the number fixes

--- hw03/src/hw03.java
+++ hw03/src/hw03.java
@@ -5,11 +5,23 @@ public class hw03 {
 
     private static int level = 10;
     private static int attemptsCount = 3;
-    private static int number2Guess = new Random().nextInt(level);
+    private static Random rnd = new Random();
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int repeatGame = 0;
+        do{
+            playTheGame();
+
+            repeatGame = getNumberFromScanner("Повторить игру еще раз? 1 – да / 0 – нет", 0,1);
+
+        }while (repeatGame == 1);
+        sc.close();
+    }
+
+    public static void playTheGame(){
         boolean win = false;
+        int number2Guess = rnd.nextInt(level);
         for (int i = 0; i < attemptsCount; i++) {
             int userAnswer = getNumberFromScanner("Угадайте число от 0 до " + (level - 1), 0, 9);
             if (number2Guess == userAnswer){
@@ -25,7 +37,6 @@ public class hw03 {
         if(!win){
             System.out.println("Вы так и не угадали число " + number2Guess + "\nВ следующий раз Вам точно повезет!");
         }
-        sc.close();
     }
 
     public static int getNumberFromScanner(String message, int min, int max) {
@@ -33,7 +44,7 @@ public class hw03 {
         do {
             System.out.println(message);
             x = sc.nextInt();
-        } while (x <= min || x >= max);
+        } while (x < min || x > max);
 
         return x;
     }
