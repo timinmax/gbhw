10e1c03ee64953107218f2051799afa3fb03ae20
Maxim Timinskiy <timinmax@gmail.com>
2019-12-23 00:14:05 +0300

    final

--- hw06/src/hw06.java
+++ hw06/src/hw06.java
@@ -1,5 +1,16 @@
 public class hw06 {
     public static void main(String[] args) {
+        Cat catMartin = new Cat("brown", "Martin");
+        catMartin.sayHello();
+        catMartin.run(300);
+        catMartin.run(140);
+        catMartin.jump(1);
+        catMartin.swim(10);
 
+        Dog dogLucky = new Dog("white", "Lucky");
+        dogLucky.sayHello();
+        dogLucky.run(1000);
+        dogLucky.run(400);
+        dogLucky.swim(4);
     }
 }
