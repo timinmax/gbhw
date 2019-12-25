d3ba47c7dc9769e695533204ef9bafc8c249026f
Maxim Timinskiy <timinmax@gmail.com>
2019-12-25 23:39:28 +0300

    final

--- hw07/src/Plate.java
+++ hw07/src/Plate.java
@@ -1,17 +1,27 @@
 public class Plate {
 
-    private int food;
+    private int calories;
 
-    public Plate(int food) {
-        this.food = food;
+    public Plate(int calories) {
+
+        this.calories = calories;
     }
 
-    public void decreaseFood(int minus) {
-        food -= minus;
+    public int decreaseFood(int minus) {
+        int accessibleCalories = Math.min(minus, this.getCalories());
+        this.calories -= accessibleCalories;
+        return accessibleCalories;
+    }
+    public int getCalories(){
+        return this.calories;
+    }
+
+    public void addCalories(int caloriesCount){
+        this.calories += caloriesCount;
     }
 
     public void info() {
-        System.out.println("Plate: " + food);
+        System.out.println("Plate: " + calories);
     }
 
 }
--- hw07/src/hw07.java
+++ hw07/src/hw07.java
@@ -1,9 +1,24 @@
 public class hw07 {
     public static void main(String[] args) {
-        Cat cat = new Cat("Pixel", 200);
-        Plate plate = new Plate(1000);
+        Cat[] Cats = new Cat[2];
+        Cats[0] = new Cat("Tom");
+        Cats[1] = new Cat("Leo");
+
+        Plate plate = new Plate(10000);
+        System.out.println("Инфо перед едой:");
         plate.info();
-        cat.eat(plate);
+        for (int i = 0; i<Cats.length; i++){
+            System.out.println(Cats[i].toString());
+        }
+
+        for (int i = 0; i<Cats.length; i++){
+            Cats[i].eat(plate);
+        }
+
+        System.out.println("После еды");
         plate.info();
+        for (int i = 0; i<Cats.length; i++){
+            System.out.println(Cats[i].toString());
+        }
     }
 }
