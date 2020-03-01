994ac5bba745c1b3b702b91f1e311b3fc206faa9
Maxim Timinskiy <timinmax@gmail.com>
2020-03-01 23:02:13 +0300

    task1

--- hw0203/src/hw0203.java
+++ hw0203/src/hw0203.java
@@ -1,5 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class hw0203 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String[] demoArr = {"hello", "London", "is", "the", "capital", "of", "GB",
+                            "my", "name", "is", "Max", "he", "is", "the", "capital"};
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < demoArr.length; i++){
+            set.add(demoArr[i]);
+        }
+
+        System.out.println(set);
+
     }
 }
