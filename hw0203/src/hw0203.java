bc034e63aa0dbe29ab78ec254509552b3ab00078
Maxim Timinskiy <timinmax@gmail.com>
2020-03-01 23:44:27 +0300

    task2 upgrade 2

--- hw0203/src/hw0203.java
+++ hw0203/src/hw0203.java
@@ -1,11 +1,10 @@
 import java.util.HashSet;
-import java.util.Set;
 
 public class hw0203 {
     public static void main(String[] args) {
         String[] demoArr = {"hello", "London", "is", "the", "capital", "of", "GB",
                             "my", "name", "is", "Max", "he", "is", "the", "capital"};
-        Set<String> set = new HashSet<>();
+        HashSet<String> set = new HashSet<>();
 
         for (int i = 0; i < demoArr.length; i++){
             set.add(demoArr[i]);
--- hw0203/src/phonebook.java
+++ hw0203/src/phonebook.java
@@ -1,17 +1,16 @@
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
 
 public class phonebook {
-    private HashMap<String, Set> phoneBookRecords = new HashMap<>();
+    private HashMap<String, HashSet> phoneBookRecords = new HashMap<>();
 
-    public Set get(String secondName){
-        Set<String> phBookRecord = phoneBookRecords.get(secondName);
+    public HashSet get(String secondName){
+        HashSet<String> phBookRecord = phoneBookRecords.get(secondName);
         return phBookRecord == null ? new HashSet<String>():phBookRecord;
     }
 
     public void add(String secondName, String phoneNumber){
-        Set<String> phBookRecord = this.get(secondName);
+        HashSet<String> phBookRecord = this.get(secondName);
         phBookRecord.add(phoneNumber);
         phoneBookRecords.put(secondName, phBookRecord);
     }
