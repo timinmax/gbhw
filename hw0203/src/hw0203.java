7643a4e0875710644f72b442dc3ebe88ecc70eff
Maxim Timinskiy <timinmax@gmail.com>
2020-03-09 11:38:06 +0300

    word count fix

--- hw0203/src/hw0203.java
+++ hw0203/src/hw0203.java
@@ -1,17 +1,21 @@
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class hw0203 {
     public static void main(String[] args) {
         String[] demoArr = {"hello", "London", "is", "the", "capital", "of", "GB",
                             "my", "name", "is", "Max", "he", "is", "the", "capital"};
-        HashSet<String> set = new HashSet<>();
 
+        /*HashSet<String> set = new HashSet<>();
         for (int i = 0; i < demoArr.length; i++){
             set.add(demoArr[i]);
         }
-
         System.out.println(set);
-
+        */
+        HashMap<String,Integer> counter = new HashMap<>();
+        for (int i = 0; i < demoArr.length; i++){
+            counter.compute(demoArr[i], (word,count)->(count == null)? 1: count + 1);
+        }
+        System.out.println(counter);
 
         phonebook pb = new phonebook();
         pb.add("Ivanov", "89032489416");
