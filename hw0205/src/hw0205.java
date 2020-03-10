3e9d3e020781c450fce5ea422d62a0bb4388ebd2
Maxim Timinskiy <timinmax@gmail.com>
2020-03-10 19:17:56 +0300

    oneThreadCalc ready

--- hw0205/src/hw0205.java
+++ hw0205/src/hw0205.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 public class hw0205 {
+    static final int SIZE = 10000000;
+    static final int HALF = SIZE / 2;
+
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("1 thread calc: " + oneThreadCalc() + " ms.");
+    }
+
+    public static long oneThreadCalc(){
+
+        float[] arr = new float[SIZE];
+        Arrays.fill(arr, 1f);
+        long a = System.currentTimeMillis();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        return System.currentTimeMillis() - a;
     }
 }
