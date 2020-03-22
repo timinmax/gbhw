f8ff40b9900ea8fd0514a2090cdb9de61a9371fc
Maxim Timinskiy <timinmax@gmail.com>
2020-03-22 13:23:14 +0300

    personal messages ready

--- GeekBrains_Java2_2020_02_20-lesson7/NetworkServer/src/ru/geekbrains/java2/server/NetworkServer.java
+++ GeekBrains_Java2_2020_02_20-lesson7/NetworkServer/src/ru/geekbrains/java2/server/NetworkServer.java
@@ -55,7 +55,13 @@ public class NetworkServer {
             }
         }
     }
-
+    public void personalMessage(String recieverNickname, String messageText) throws IOException {
+        for (ClientHandler client : clients) {
+            if (client.getNickname().toLowerCase().trim().equals(recieverNickname.toLowerCase().trim())) {
+                client.sendMessage(messageText);
+            }
+        }
+    }
     public synchronized void subscribe(ClientHandler clientHandler) {
         clients.add(clientHandler);
     }
@@ -63,4 +69,6 @@ public class NetworkServer {
     public synchronized void unsubscribe(ClientHandler clientHandler) {
         clients.remove(clientHandler);
     }
+
+
 }
